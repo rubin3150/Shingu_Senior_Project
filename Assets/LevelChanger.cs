01cc59342a86b4c72f8d841a9f3b1eaada425b83
r32sqbchil <91232917+r32sqbchil@users.noreply.github.com>
2022-04-26 11:47:25 +0900

    Fix: 필요 없는 씬,스크립트 수정,제거 및 Fade 시스템 재구축
    - 기존 Fade 시스템이 Fade Out에 실패하여 애니메이션을 활용하여 다시 만듦
    
    - Scene들을 Prefab으로 대체하였기 때문에 기존씬들을 차차 제거해나가고 있음

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        //SceneManager.LoadScene(levelToLoad);
+    }
+}
