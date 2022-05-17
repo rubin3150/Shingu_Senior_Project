3f91f28d47380e6b9ee829e35c9ccf03dc0466fc
rubin3150 <103229133+rubin3150@users.noreply.github.com>
2022-05-17 12:07:22 +0900

    Feature: 캐릭터 이동방식 변경
    - 기존에 캐릭터가 천천히 이동하는 방식에서 순간 이동하는 방식으로 변경

---
+++ Assets/6.test/kjw/Scripts/CharacterSystem/CharacterTeleporting.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTeleporting : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+    private bool isCharaterMove;
+    private int wallLayerMask = 1 << 6;
+    private int unitLayerMask = 1 << 7;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            Teleporting();
+
+        LookMoveDirection();
+    }
+
+    public void Teleporting()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, float.MaxValue))
+        {
+            if(hit.transform.tag == "Unit")
+            {
+                return;
+            }
+        }
+
+        if (Physics.Raycast(ray, out hit, 50f, wallLayerMask))
+        {
+            this.transform.position = new Vector3(hit.transform.position.x, transform.position.y, hit.transform.position.z);
+            isCharaterMove = true;
+        }
+    }
+
+    private void LookMoveDirection()
+    { 
+        if (isCharaterMove) 
+        {
+            
+        } 
+    }
+}
