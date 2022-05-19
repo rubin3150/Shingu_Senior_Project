35229d354aef38622d7f0db430fe4d28b8840c45
r32sqbchil <91232917+r32sqbchil@users.noreply.github.com>
2022-05-19 11:12:59 +0900

    Add: 버튼 기능 추가

--- Assets/6.test/ayc/UIController.cs
+++ Assets/6.test/ayc/UIController.cs
@@ -4,19 +4,21 @@ public class UIController : Singleton<UIController>
 {
     public GameObject[] pagePrefab;
 
-    public int maxPageNumber;
-
     [HideInInspector] public int pageNumber = 0;
 
-    public void NextPage()
+    public void NextPage(int _int)
     {
-        if(pageNumber < maxPageNumber) {
-            pagePrefab[pageNumber].gameObject.SetActive(false);
-            pageNumber += 1;
-            pagePrefab[pageNumber].gameObject.SetActive(true);
-        }
-        else {
-            return;
-        }
+        pagePrefab[pageNumber].gameObject.SetActive(false);
+        pageNumber = _int;
+        pagePrefab[pageNumber].gameObject.SetActive(true);
+    }
+
+    public void ExitPage()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
