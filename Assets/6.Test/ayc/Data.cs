fc1efc381b814137fb8710424ae83b403f7172cb
r32sqbchil <91232917+r32sqbchil@users.noreply.github.com>
2022-06-13 18:01:25 +0900

    Fix: 학교끝나고 커밋

---
+++ Assets/6.Test/ayc/Data.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Data : Singleton<Data>
+{
+    public string nicknameBasket; //바구니
+    public TextMeshProUGUI nicknamefield; //유저입력
+    public TextMeshProUGUI nicknameshow; //보여줄거
+    //public TextMeshProUGUI nicknameData;
+
+    public void SaveNicknameData()
+    {
+        nicknameBasket = nicknamefield.text;
+        nicknameshow.text = nicknameBasket;
+    }
+}
---
+++ Assets/6.Test/ayc/tempNickname.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class tempNickname : MonoBehaviour
+{
+    // private string nicknameData;
+    // private string nicknameData;
+    
+    // public void OnEnable()
+    // {
+        // Debug.Log("메인맵 텍스트 " + nicknameData.text + "  이전 데이터 " + Data.Instance.nicknameBasket);
+        // Debug.Log(11);
+        // Debug.Log(Data.Instance.nicknameBasket);
+        //nicknameData.text = Data.Instance.nicknameBasket;
+    // }
+
+    // private void Update()
+    // {
+    //     if(Input.GetKeyDown(KeyCode.R))
+    //     {
+    //         Debug.Log(Data.Instance.nicknameBasket);
+    //     }
+    // }
+}
