8dc7a443d8ffa8bb4ca430f2a1b333a67e8dc4ef
r32sqbchil <91232917+r32sqbchil@users.noreply.github.com>
2022-06-16 14:34:31 +0900

    Fix: 그래픽팀과 UI 위치 및 색깔 수정
    - Fade 효과 추가

--- Assets/6.Test/ayc/Data.cs
+++ Assets/6.Test/ayc/Data.cs
@@ -8,11 +8,11 @@ public class Data : Singleton<Data>
     public string nicknameBasket; //바구니
     public TextMeshProUGUI nicknamefield; //유저입력
     public TextMeshProUGUI nicknameshow; //보여줄거
-    //public TextMeshProUGUI nicknameData;
 
     public void SaveNicknameData()
     {
         nicknameBasket = nicknamefield.text;
+
         nicknameshow.text = nicknameBasket;
     }
 }
