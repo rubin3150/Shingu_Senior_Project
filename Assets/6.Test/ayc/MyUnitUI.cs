67551287e20893facf64b8c63b4065b27ac230ee
r32sqbchil <91232917+r32sqbchil@users.noreply.github.com>
2022-06-19 20:35:50 +0900

    Add: 모든 UI작업 완료

---
+++ Assets/6.Test/ayc/MyUnitUI.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyUnitUI : MonoBehaviour
+{
+    public GameObject[] matching;
+    public Button[] myunitBtn;
+
+
+    void Start()
+    {
+        myunitBtn[0].onClick.AddListener(() => UnitMatching(0));
+        myunitBtn[1].onClick.AddListener(() => UnitMatching(1));
+        myunitBtn[2].onClick.AddListener(() => UnitMatching(2));
+        myunitBtn[3].onClick.AddListener(() => UnitMatching(3));
+        myunitBtn[4].onClick.AddListener(() => UnitMatching(4));
+        myunitBtn[5].onClick.AddListener(() => UnitMatching(5));
+        myunitBtn[6].onClick.AddListener(() => UnitMatching(6));
+    }
+
+    public void UnitMatching(int _int)
+    {
+        for(int i=0; i<matching.Length; i++)
+        {
+            matching[i].SetActive(false);
+        }
+        matching[_int].SetActive(true);
+
+        Data.Instance.sfx.clip = Data.Instance.btnClip;
+        Data.Instance.sfx.Play();
+    }
+}
--- Assets/6.Test/ayc/WorldMapUI.cs
+++ Assets/6.Test/ayc/WorldMapUI.cs
@@ -17,6 +17,7 @@ public class WorldMapUI : MonoBehaviour
     1. 내 유닛 아이콘
     2. 환경 설정 아이콘
     3. 상점 - 닫기 버튼
+    4. 내 유닛 - 닫기 버튼
     */
 
     void Start()
@@ -25,9 +26,10 @@ public class WorldMapUI : MonoBehaviour
         Data.Instance.mainAudio.Play();
 
         worldMapBtn[0].onClick.AddListener(() => PopUpOnController(0, false));
-        worldMapBtn[1].onClick.AddListener(() => PopUpOnController(1, true));
+        worldMapBtn[1].onClick.AddListener(() => PopUpOnController(1, false));
         worldMapBtn[2].onClick.AddListener(() => PopUpOnController(2, false));
         worldMapBtn[3].onClick.AddListener(() => PopUpOffController(0, true));
+        worldMapBtn[4].onClick.AddListener(() => PopUpOffController(1, true));
     }
 
     public void PopUpOnController(int _int, bool _bool)
